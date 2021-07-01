738938f10d74e2dcf78491b8e70c6a5548f4ec00
Dung Tran <pdung1989@gmail.com>
2021-07-01 11:04:51 +0300

    add Meals components

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,14 @@
+import { Fragment } from 'react';
+import MealsSummary from './MealsSummary';
+import AvailableMeals from './AvailableMeals';
+
+const Meals = () => {
+  return (
+    <Fragment>
+      <AvailableMeals />
+      <MealsSummary />
+    </Fragment>
+  );
+};
+
+export default Meals;
