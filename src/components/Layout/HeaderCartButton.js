4592f2ff668bbd3a705e709f63951bc4d2feb2c3
Dung Tran <pdung1989@gmail.com>
2021-07-11 15:46:31 +0300

    use context in HeadercartButton to update the amount of meals

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,25 @@
+import { useContext } from 'react';
+
 import CardIcon from '../Cart/CartIcon';
+import CartContext from '../../store/cart-context';
 import classes from './HeaderCartButton.module.css';
 
-const HeaderCartButton = props => {
+const HeaderCartButton = (props) => {
+  //this component will re-evaluate when ever the context changes
+  const cartCtx = useContext(CartContext);
+
+ //each item include different number amount of meals
+  const numberOfCartItems = cartCtx.items.reduce((curNum, item) => {
+    return curNum + item.amount;
+  }, 0);
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CardIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>0</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   )
 };
