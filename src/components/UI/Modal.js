e57ffdf39d8f6bc5cf01611acc4ce3e196709b40
Dung Tran <pdung1989@gmail.com>
2021-07-11 14:15:46 +0300

    create modal component which include backdrop and modelOverlay

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import classes from './Modal.module.css';
 
+const Backdrop = props => {
+  return <div classes={classes.backdrop} />;
+};
+
+const ModalOverlay = props => {
+  return (
+    <div classes={classes.modal}>
+      <div classes={classes.content}>
+        {props.children}
+      </div>
+    </div>
+  )
+}
+
 const Modal = (props) => {
   return (
+    <Fragment>
+      <Backdrop />
+      <ModalOverlay />
+    </Fragment>
 
   );
 };
