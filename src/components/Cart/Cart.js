1392b7d2fff9774c5b377ea8873f16a61f03a3fd
Dung Tran <pdung1989@gmail.com>
2021-07-22 10:51:54 +0300

    edit style for the checkout and reading form value with useRef()

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,10 @@
-import { useContext, useState }  from 'react';
+import { useContext, useState } from 'react';
 
 import CartItem from './CartItem';
 import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
 import CartContext from '../../store/cart-context';
-import CheckoutForm from './CheckoutForm';
+import Checkout from './Checkout';
 
 const Cart = (props) => {
   const [isCheckout, setIsCheckout] = useState();
@@ -21,7 +21,7 @@ const Cart = (props) => {
   };
   //add item
   const cartItemAddHandler = item => {
-    cartCtx.addItem({...item, amount: 1})
+    cartCtx.addItem({ ...item, amount: 1 })
   };
 
   //order process
@@ -30,17 +30,17 @@ const Cart = (props) => {
   };
 
   const cartItems = (
-  <ul className={classes['cart-items']}>
-    {cartCtx.items.map(item => (
-      <CartItem 
-        key={item.id}
-        name={item.name}
-        amount={item.amount}
-        price={item.price}
-        onRemove={cartItemRemoveHandler.bind(null, item.id)}
-        onAdd={cartItemAddHandler.bind(null, item)}/>
-    ))}
-  </ul>);
+    <ul className={classes['cart-items']}>
+      {cartCtx.items.map(item => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)} />
+      ))}
+    </ul>);
   return (
     <Modal onClose={props.onClose}>
       {cartItems}
@@ -48,13 +48,13 @@ const Cart = (props) => {
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
-      {isCheckout && <CheckoutForm onCancel={props.onClose}/>}
+      {isCheckout && <Checkout onCancel={props.onClose} />}
       <div className={classes.actions}>
         {!isCheckout && <button className={classes['button--alt']} onClick={props.onClose}>Close</button>}
         {hasItems && !isCheckout && <button className={classes.button} onClick={onOrderHandler}>Order</button>}
       </div>
     </Modal>
-    
+
   );
 };
 
